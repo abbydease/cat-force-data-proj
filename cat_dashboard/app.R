e87a8c8c41efede0dc16f49d032a303129badb37
abdease <adease@smith.edu>
2026-01-14 18:03:52 -0500

    initial commit

---
+++ cat_dashboard/app.R
@@ -0,0 +1,112 @@
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library(readxl)
+library(bslib)
+
+cat_force_pivot <- read_excel("/Users/abigaildease/Desktop/cat-force-data-proj/data/CAT Pivoted Force Data.xlsx")
+
+
+ui <- page_navbar(
+
+   title = "Carolina Aquatic Team Data Dashboard",
+   theme = bs_theme(version = 5, primary = "#1a91d6"),
+   
+   #individual swimmer panel
+   nav_panel(
+     "Individual",
+    page_sidebar(
+      sidebar = sidebar(
+           selectInput("selected_swimmer", "Athlete:",
+                       choices = sort(unique(cat_force_pivot$Name))),
+           selectInput("selected_session", "Session:",
+                       choices = sort(unique(cat_force_pivot$Session)),
+                       multiple = TRUE)
+           
+       ),
+      layout_columns(
+        card(
+          card_header(textOutput("swimmer_overview_title")),
+          plotOutput("sessionPlot")
+           ),
+        card(
+          card_header("Session Averages"),
+          tableOutput("avgTable")
+        )
+         )
+       )
+     ),
+   nav_panel(
+     "Team",
+     page_sidebar = sidebar(
+       selectInput("selected_gender", "Men/Women:",
+                   choices = sort(unique(cat_force_pivot$Gender)),
+                   multiple = TRUE),
+       selectInput("selected_session", "Session:",
+                   choices = sort(unique(cat_force_pivot$Session)),
+                   multiple = TRUE)
+     ),
+     layout_columns(
+       card(
+         card_header(textOutput("team_overview_title")),
+         plotOutput("teamPlot")
+       )
+     )
+   )
+)
+    
+
+
+
+server <- function(input, output, session) {
+  
+  output$swimmer_overview_title <- renderText({
+    req(input$selected_swimmer)
+    paste("Force Data for:", input$selected_swimmer)
+  })
+  
+  output$team_overview_title <- renderText({
+    req(input$selected_session)
+    paste("Team Force Data over Session(s)", input$selected_session)
+  })
+
+  filtered_data_main <- reactive({
+    req(input$selected_swimmer, input$selected_session)
+    
+    cat_force_pivot %>%
+      filter(Name == input$selected_swimmer) %>%
+      filter(Session %in% input$selected_session)
+  })
+  
+  
+  output$sessionPlot <- renderPlot({
+    req(nrow(filtered_data_main()) > 0)
+    
+    ggplot(filtered_data_main(), aes(x = Attempt, y = Force, color = as.factor(Session))) +
+      geom_line() +
+      geom_point() +
+      theme_minimal() +
+      labs(color = "Session")
+  })
+  
+    athlete_averages <- reactive({
+      req(filtered_data_main())
+      
+      filtered_data_main() %>%
+        group_by(Name, Session) %>%
+        summarize(
+          avg_force = mean(Force, na.rm = TRUE),
+          max_force = max(Force, na.rm = TRUE),
+          .groups = "drop"
+        )
+    })
+    
+    output$avgTable <- renderTable({
+      athlete_averages()
+    })
+
+
+}
+
+
+shinyApp(ui = ui, server = server)
